2f6014a15fdcf3ae66b9c3d3fcb920c8b0ec2b21
svetultus <svetultus@gmail.com>
2019-08-09 19:41:10 +0300

    functions

--- src/UseEffectHook/UseEffectHook.js
+++ src/UseEffectHook/UseEffectHook.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import {getJoke} from '../utils'
+import React, { useEffect, useState } from "react";
+import { getJoke } from "../utils";
 
 /*
   Напишите компонент, который загрузит шутку о Чаке Норрисе
@@ -8,4 +8,19 @@ import {getJoke} from '../utils'
   Вам придётся использовать асинхронный эффект. Это имеет свои особенности.
 */
 
-export const UseEffectHook = () => { }
+export const UseEffectHook = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getJoke();
+
+      setData(result.value);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("data", data);
+  return <div data-testid="joke">{data}</div>;
+};
